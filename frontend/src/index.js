20b67d6e05e722ed90d9009eb06dd663f2f804d7
Ravi-047 <ravi047raj@gmail.com>
2023-01-19 12:25:19 +0530

    fp04_179_Day-02

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-
   <BrowserRouter>
     <ChakraProvider>
       <App />
